818951e98290151025a561a07a81909b18c9105d
alekseyvotencev <votencevaleksejj@gmail.com>
2023-04-28 18:45:36 +0500

    some pages corrected + mobile menu added

--- src/js/main.js
+++ src/js/main.js
@@ -40,9 +40,188 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // поиск в хедере
     const headerSearch = document.querySelector('.header__search');
-    headerSearch.addEventListener('click', function () {
+    const inputSearch = headerSearch.querySelector('.header__search-form__input');
+    const headerCatalog = header.querySelector('.header__catalog');
+    const headerClients = header.querySelector('.header__clients');
+    const headerContacts = header.querySelector('.header__contacts');
+    const headerLocation = header.querySelector('.header__location');
+    const headerSearchClear = header.querySelector('.header__search-clear');
+    const headerLogo = header.querySelector('.header__logo')
 
+    headerSearchClear.addEventListener('click', function () {
+        inputSearch.focus();
+        inputSearch.value = "";
     })
+
+    if (window.innerWidth > 768) {
+        inputSearch.addEventListener('focus', function () {
+            headerCatalog.classList.add('disable');
+            headerClients.classList.add('disable');
+            headerContacts.classList.add('disable');
+            headerLocation.classList.add('disable');
+            headerSearch.classList.add('active');
+        })
+
+        inputSearch.addEventListener('focusout', function () {
+            headerCatalog.classList.remove('disable');
+            headerClients.classList.remove('disable');
+            headerContacts.classList.remove('disable');
+            headerLocation.classList.remove('disable');
+            headerSearch.classList.remove('active');
+        })
+    } else {
+        inputSearch.addEventListener('focus', function () {
+            headerLogo.classList.add('disable');
+            headerSearch.classList.add('active');
+        })
+
+        inputSearch.addEventListener('focusout', function () {
+            headerLogo.classList.remove('disable');
+            headerSearch.classList.remove('active');
+        })
+    }
+
+    // мобильное меню в хедере
+    const headerBurger = header.querySelector('.header__burger');
+    const headerMobileMenu = header.querySelector('.header__mobile');
+    const headerMobileBottom = header.querySelector('.header__mobile-bottom')
+    let headerCatalogPopup = header.querySelector('.header__catalog-popup');
+    headerBurger.addEventListener('click', function () {
+        headerBurger.classList.toggle('active');
+        headerMobileBottom.classList.toggle('active');
+        document.body.classList.toggle('lock');
+        document.documentElement.classList.toggle('lock');
+        headerMobileMenu.classList.toggle('active');
+        if (!headerBurger.classList.contains('active')) {
+            headerCatalogPopup.classList.remove('active');
+            headerCatalogPopup.querySelectorAll('active').forEach(item => item.classList.remove('active'));
+        }
+    })
+
+    const headerMenuBtns = header.querySelectorAll('.header__mobile-list__item-btn');
+    headerMenuBtns.forEach(btn => {
+        btn.addEventListener('click', function () {
+            let path = btn.dataset.path;
+            const elMenu = header.querySelector(`[data-target=${path}]`);
+            elMenu.classList.add('active');
+        })
+    })
+
+    // step1
+    const headerStep1Items = header.querySelectorAll('.header__catalog-popup__step1-list-item');
+    headerStep1Items.forEach(item => {
+        if (window.innerWidth > 768) {
+            item.addEventListener('mouseover', function () {
+                item.querySelector('.header__catalog-popup__step2').classList.add('active');
+            })
+
+            item.addEventListener('mouseout', function () {
+                item.querySelector('.header__catalog-popup__step2').classList.remove('active');
+            })
+        } else {
+            item.addEventListener('click', function () {
+                item.querySelector('.header__catalog-popup__step2').classList.add('active');
+            })
+        }
+    })
+
+    // step2
+    const headerStep2Items = header.querySelectorAll('.header__catalog-popup__step2-list-item');
+    headerStep2Items.forEach(item => {
+        if (window.innerWidth > 768) {
+            item.addEventListener('mouseover', function () {
+                item.querySelector('.header__catalog-popup__step3').classList.add('active');
+            })
+
+            item.addEventListener('mouseout', function () {
+                item.querySelector('.header__catalog-popup__step3').classList.remove('active');
+            })
+        } else {
+            item.addEventListener('click', function () {
+                item.querySelector('.header__catalog-popup__step3').classList.add('active');
+            })
+        }
+    })
+
+
+    // step3
+    const headerStep3Items = header.querySelectorAll('.header__catalog-popup__step3-list-item');
+    headerStep3Items.forEach(item => {
+        if (window.innerWidth > 768) {
+            item.addEventListener('mouseover', function () {
+                item.querySelector('.header__catalog-popup__step4').classList.add('active');
+            })
+
+            item.addEventListener('mouseout', function () {
+                item.querySelector('.header__catalog-popup__step4').classList.remove('active');
+            })
+        } else {
+            item.addEventListener('click', function () {
+                item.querySelector('.header__catalog-popup__step4').classList.add('active');
+            })
+        }
+    })
+
+    const headerStep3Back = header.querySelectorAll('.header__catalog-popup__step3-back');
+    headerStep3Back.forEach(btn => {
+        btn.addEventListener('click', function () {
+            btn.parentElement.classList.remove('active');
+        })
+    })
+
+    // step4
+    const headerStep4Items = header.querySelectorAll('.header__catalog-popup__step4-list-item');
+    headerStep4Items.forEach(item => {
+        if (window.innerWidth > 768) {
+            item.addEventListener('mouseover', function () {
+                item.querySelector('.header__catalog-popup__step5').classList.add('active');
+            })
+
+            item.addEventListener('mouseout', function () {
+                item.querySelector('.header__catalog-popup__step5').classList.remove('active');
+            })
+        } else {
+            item.addEventListener('click', function () {
+                item.querySelector('.header__catalog-popup__step5').classList.add('active');
+            })
+        }
+    })
+
+    const headerStep4Back = header.querySelector('.header__catalog-popup__step4-back');
+    headerStep4Back.addEventListener('click', function () {
+        headerStep4Back.parentElement.classList.remove('active');
+    })
+
+    // step5
+    const headerStep5Items = header.querySelectorAll('.header__catalog-popup__step5-list-item');
+    headerStep5Items.forEach(item => {
+        if (window.innerWidth > 768) {
+            item.addEventListener('mouseover', function () {
+                item.querySelector('.header__catalog-popup__step6').classList.add('active');
+            })
+
+            item.addEventListener('mouseout', function () {
+                item.querySelector('.header__catalog-popup__step6').classList.remove('active');
+            })
+        } else {
+            item.addEventListener('click', function () {
+                item.querySelector('.header__catalog-popup__step6').classList.add('active');
+            })
+        }
+    })
+
+    const headerStep5Back = header.querySelector('.header__catalog-popup__step5-back');
+    headerStep5Back.addEventListener('click', function () {
+        headerStep5Back.parentElement.classList.remove('active');
+    })
+
+    // step6 
+
+    const headerStep6Back = header.querySelector('.header__catalog-popup__step6-back');
+    headerStep6Back.addEventListener('click', function () {
+        headerStep6Back.parentElement.classList.remove('active');
+    })
+
 
     // город в хедере
     const headerLocationBtn = document.querySelector('.header__location-btn');
@@ -137,6 +316,21 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    // характеристики на странице КАРТОЧКА ТОВАРА
+    if (document.querySelector('.product-card__center-btn')) {
+        const productOptionsBtn = document.querySelector('.product-card__center-btn');
+        let content = document.querySelector('.product-card__center-list');
+        productOptionsBtn.addEventListener('click', function () {
+            productOptionsBtn.classList.toggle('active');
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null
+            } else {
+                content.style.maxHeight = content.scrollHeight / 10 + "rem";
+            }
+        })
+    }
+
+
     // карта на странице КОНТАКТЫ
     if (document.querySelector('#contacts-map')) {
 
@@ -178,11 +372,21 @@ document.addEventListener('DOMContentLoaded', function () {
             const contentBlocks = contentConainer.querySelectorAll('[data-target]');
             tabsBtns.forEach(btn => {
                 btn.addEventListener('click', function (e) {
-                    tabsBtns.forEach(el => el.classList.remove('active'));
-                    btn.classList.add('active');
-                    const path = e.target.dataset.path;
-                    contentBlocks.forEach(block => block.classList.remove('active'));
-                    contentConainer.querySelector(`[data-target=${path}]`).classList.add('active');
+                    tabsBtns.forEach(el => {
+                        if (el !== btn) {
+                            el.classList.remove('active')
+                        }
+                    });
+                    btn.classList.toggle('active');
+                    if (btn.classList.contains('active')) {
+                        const path = btn.dataset.path;
+                        contentBlocks.forEach(block => block.classList.remove('active'));
+                        contentConainer.querySelector(`[data-target=${path}]`).classList.add('active');
+                    } else {
+                        contentBlocks.forEach(block => block.classList.remove('active'));
+                        contentConainer.querySelector(`[data-target=default]`).classList.add('active');
+                    }
+
                 })
             })
         }
@@ -202,17 +406,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // НОВОСТИ
 
     // табы новостей 
-    initTabs(document.querySelector('.news-main .tabs'), document.querySelector('.news-main '))
+    initTabs(document.querySelector('.news-main .tabs'), document.querySelector('.news-main'))
 
     // длина новости (добавляется многоточие в конце при переполнении)
 
-    if (document.querySelector('.news-main__list-item__center-description')) {
-        const newsDescriptions = document.querySelectorAll('.news-main__list-item__center-description');
-        newsDescriptions.forEach(desc => {
-            if (desc.innerText.length > 198) {
-                desc.innerText = desc.innerText.substring(0, 198);
-                desc.innerText += '...';
-            }
-        })
-    }
+    // if (document.querySelector('.news-main__list-item__center-description')) {
+    //     const newsDescriptions = document.querySelectorAll('.news-main__list-item__center-description');
+    //     newsDescriptions.forEach(desc => {
+    //         if (desc.innerText.length > 198) {
+    //             desc.innerText = desc.innerText.substring(0, 198);
+    //             desc.innerText += '...';
+    //         }
+    //     })
+    // }
 })
